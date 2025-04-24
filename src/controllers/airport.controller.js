b74b2e7b4e45138ade56a052998974cf7c09e4d5
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-04-24 11:01:39 +0600

    Implement filtering on airport controller

--- src/controllers/airport.controller.js
+++ src/controllers/airport.controller.js
@@ -20,21 +20,47 @@ const createAirport = async (req, res) => {
 
 
 const getAllAirport = async (req, res) => {
-    try {
-        const data = await prisma.airport.findMany()
+    const { airport } = req.query || {};
 
-        res.status(200).json({
-            succes: true,
-            message: "Airport Get Successfull",
-            data
-        })
+    try {
+        let data = [];
+
+        const findByField = async (field) => {
+            return await prisma.airport.findMany({
+                where: {
+                    [field]: {
+                        contains: airport
+                    },
+                },
+            });
+        };
+
+        if (airport) {
+            const priorityFields = ['airportCode', 'airportName', 'countryCode', 'countryName'];
+
+            for (const field of priorityFields) {
+                data = await findByField(field);
+                if (data.length > 0) break;
+            }
+        } else {
+            data = await prisma.airport.findMany();
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Airport fetch successful",
+            data,
+        });
+
     } catch (error) {
-        res.status(500).json({
-            succes: false,
-            message: "There was a server side error"
-        })
+        return res.status(500).json({
+            success: false,
+            message: "There was a server-side error",
+        });
     }
-}
+};
+
+
 
 
 
